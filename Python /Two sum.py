2d79b939b7b45241473af2a1d034989cf8c925ae
bashar37888 <87509746+bashar37888@users.noreply.github.com>
2024-10-25 01:40:34 +0530

    Update Two sum.py

--- Python /Two sum.py
+++ Python /Two sum.py
@@ -1,1 +1,13 @@
+nums = [1, 2, 3, 4]
+print(nums)
+Target = int(input("Target:"))
 
+
+for i in range(len(nums)):
+  for k in range(i, len(nums)):
+    if i == k:
+      continue
+      
+    if nums[i] + nums[k] == Target:
+      print([i, k])
+      break
